156d641d6f93f7d03fcbcaf63aaa4afcbbc49d7a
ABotelho23 <botelho2305@gmail.com>
2019-02-28 22:46:21 -0500

    add automatic mok cert creation refer to https://computerlinguist.org/make-dkms-sign-kernel-modules-for-secure-boot-on-ubuntu-1604.html

---
+++ autoMok.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#create generic conf file
+echo 'POST_BUILD=../../../../../../root/sign-kernel.sh' >> /etc/dkms/sign-kernel-objects.conf
+
+
+echo '#!/bin/bash
+
+cd ../$kernelver/$arch/module/
+
+for kernel_object in *ko; do
+     echo "Signing kernel_object: $kernel_object"
+    /usr/src/linux-headers-$kernelver/scripts/sign-file sha256 /root/MOK.priv /root/MOK.der "$kernel_object";
+done' >> /root/sign-kernel.sh
+
+ln -s /etc/dkms/sign-kernel-objects.conf /etc/dkms/virtualbox.conf
+ln -s /etc/dkms/sign-kernel-objects.conf /etc/dkms/wireguard.conf
