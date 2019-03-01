93209a8472372b88918e64558f4693e6dbb012bc
ABotelho23 <botelho2305@gmail.com>
2019-03-01 18:07:01 -0500

    add auto module signing into main script

--- setupScript.sh
+++ setupScript.sh
@@ -342,6 +342,22 @@ echo 'source /etc/profile.d/vte.sh' >> $HOME/.bashrc
 echo 'fi' >> $HOME/.bashrc
 ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
 
+#autosign
+#create generic conf file
+sudo echo 'POST_BUILD=../../../../../../root/sign-kernel.sh' >> /etc/dkms/sign-kernel-objects.conf
+
+sudo echo '#!/bin/bash
+
+cd ../$kernelver/$arch/module/
+
+for kernel_object in *ko; do
+     echo "Signing kernel_object: $kernel_object"
+    /usr/src/linux-headers-$kernelver/scripts/sign-file sha256 /root/MOK.priv /root/MOK.der "$kernel_object";
+done' >> /root/sign-kernel.sh
+
+sudo ln -s /etc/dkms/sign-kernel-objects.conf /etc/dkms/virtualbox.conf
+sudo ln -s /etc/dkms/sign-kernel-objects.conf /etc/dkms/wireguard.conf
+
 #clean up
 sudo apt autoremove && sudo apt clean
 
