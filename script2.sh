3dea1753fb068d9dd8484a4ccfd1627152825faa
Alex Botelho <botelho2305@gmail.com>
2019-08-27 18:13:32 -0400

    Comment out Google cursor and remove missing plank repo

--- script2.sh
+++ script2.sh
@@ -273,19 +273,15 @@ sudo rm -r /usr/share/plank/themes/setup-git-v1.sh
 sudo rm -r /usr/share/plank/themes/git-v1.sh
 sudo rm -r /usr/share/plank/themes/README.md
 
-#more Plank themes
-git clone git://github.com/LinxGem33/Plank-Themes.git
-sudo cp -r ./Plank-Themes/'Plank Themes'/* /usr/share/plank/themes
-
 #Google cursor theme
-sudo apt-get install x11-apps -y
-git clone git://github.com/KaizIqbal/Google_Cursor.git
-cd Google_Cursor/
-chmod +x build.sh
-./build.sh
-chmod +x ./Installer_Google.sh
-sudo ./Installer_Google.sh
-cd ..
+#sudo apt-get install x11-apps -y
+#git clone git://github.com/KaizIqbal/Google_Cursor.git
+#cd Google_Cursor/
+#chmod +x build.sh
+#./build.sh
+#chmod +x ./Installer_Google.sh
+#sudo ./Installer_Google.sh
+#cd ..
 
 #wireshark install near the end cause graphical
 sudo apt-get install wireshark -y
