b7390f780f54170039bf5ce91878dc5a7399e475
Alex Botelho <botelho2305@gmail.com>
2020-04-03 20:36:48 -0400

    Major changes
    A lot of stuff that was necessary for Cinnamon no longer needed
    A lot of stuff that was necessary to strip from Server install no longer needed
    Custom Plymouth no longer needed, 20.04 comes with flicker-free boot

--- script1.sh
+++ script1.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-echo "This script installs and set up the Cinnamon Desktop Environment and cleans up Ubuntu Server for Desktop use."
+echo "This script cleans up the installation."
 read -r -p "Are you sure you want to run this script? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
 
+#disable auto updates/upgrades check
 systemctl disable --now apt-daily{,-upgrade}.{timer,service}
 sudo killall dpkg
 
@@ -11,50 +12,23 @@ sudo killall dpkg
 sudo apt-get update
 sudo apt-get upgrade -y
 
-#Cinnamon stable PPA
-sudo add-apt-repository ppa:embrosyn/cinnamon -y
-sudo apt-get update
-
-#Cinnamon core
-sudo apt-get install cinnamon-core slick-greeter lightdm-settings blueberry plymouth -y
-
-#Remove and replace cloud-init
-sudo rm /etc/netplan/*
-
-#managed by NetworkManager
-echo 'network:
-  version: 2
-  renderer: NetworkManager' | sudo tee /etc/netplan/51-netcfg.yaml
-
-sudo apt-get remove cloud-init -y
-sudo rm /etc/profile.d/Z99-cloudinit-warning.sh
-sudo rm /etc/profile.d/Z99-cloud-locale-test.sh
-sudo rm /etc/profile.d/Z97-byobu.sh
-
-#Remove Ubuntu Server stuff
-sudo apt-get remove ubuntu-server modemmanager lxd lxcfs grub-legacy-ec2 cloud-guest-utils cloud-initramfs-* landscape-common ubuntu-advantage-tools unattended-upgrades networkd-dispatcher -y
+#Remove SNAPS!
 sudo apt remove --purge snapd -y
+
+#Disable pppd-dns service
 sudo systemctl disable pppd-dns.service
 
-echo 'GRUB_DEFAULT=0
-GRUB_TIMEOUT_STYLE=hidden
-GRUB_TIMEOUT=5
-GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`
-GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"
-GRUB_CMDLINE_LINUX=""' > /etc/default/grub #enable quiet splash
-sudo update-grub
+# Re-add Gnome store, remove snap plugin
+sudo apt install gnome-software -y
+sudo apt remove gnome-software-plugin-snap
 
-sudo apt-get install plymouth-themes -y
-sudo cp -R spinfinity-custom /usr/share/plymouth/themes
-sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/spinfinity-custom/spinfinity-custom.plymouth 100
-sudo update-initramfs -u
-
+#cleanup packages
 sudo apt autoremove -y
 
 echo "Please ensure to run script2.sh upon reboot."
 
 #prompt
-read -r -p "Cinnamon installation COMPLETE! REBOOT NOW? [y/N] " rebootnow
+read -r -p "Initial setup COMPLETE! REBOOT NOW? [y/N] " rebootnow
 if [[ "$rebootnow" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
 echo "Rebooting!"
